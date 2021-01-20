882224ea3629f1fbafd477ae73b373445cabff31
Дима <kitaykin9808@gmail.com>
2021-01-20 18:47:21 +0300

    Добавьте файлы проекта.

---
+++ simplecode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace simplecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите программу, вычисляющую среднюю арифметическую двух чисел.");
+            Console.Write("Первое число = ");                                                           var OneNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Второе число = ");                                                           var TwoNumber = Convert.ToDouble(Console.ReadLine());
+                                                                                                        var MediumArithmetic = (OneNumber + TwoNumber) / 2;
+            Console.WriteLine($"Средняя арифметическая = {MediumArithmetic}");
+        }
+    }
+}
